cb9c0ef764ae853be07319911e2f9b0075b1b4c9
Aslam <aslam.saidu@gmail.com>
2016-10-21 01:18:16 +0530

    main url updated

--- src/blog_demo/urls.py
+++ src/blog_demo/urls.py
@@ -2,9 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'apps.core.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+
     url(r'^app/v1/blogs/', include('apps.blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
